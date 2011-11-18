aae1defe6fd341ec3c97af42e8ede20aafdec610
Anthony Grimes <i@raynes.me>
2011-11-18 15:02:52 -0600

    Handle 204.

--- src/tentacles/core.clj
+++ src/tentacles/core.clj
@@ -11,17 +11,20 @@
         (for [[k v] entries]
           [(.replace (name k) "-" "_") v])))
 
+(defn safe-parse [req]
+  (when-not (= 204 (:status req))
+    (json/parse-string (:body req) true)))
+
 (defn make-request [method end-point positional query]
   (let [req {:url (str url (apply format end-point positional))
              :basic-auth (query "auth")
              :method method}]
-    (-> (http/request
-         (let [proper-query (dissoc query "auth")]
-           (if (= method :post)
-             (assoc req :body (json/generate-string proper-query))
-             (assoc req :query-params proper-query))))
-        :body
-        (json/parse-string true))))
+    (safe-parse
+     (http/request
+      (let [proper-query (dissoc query "auth")]
+        (if (= method :post)
+          (assoc req :body (json/generate-string proper-query))
+          (assoc req :query-params proper-query)))))))
 
 (defn api-call [method end-point positional query]
   (let [query (query-map query)]
