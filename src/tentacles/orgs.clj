11579537f3ee464089d979e5c4a7e902650a3f65
Anthony Grimes <i@raynes.me>
2011-11-28 12:16:43 -0600

    Basic organization API implemented.

---
+++ src/tentacles/orgs.clj
@@ -0,0 +1,30 @@
+(ns tentacles.orgs
+  "Implements the Github Orgs API: http://developer.github.com/v3/orgs/"
+  (:use [tentacles.core :only [api-call]]))
+
+(defn user-orgs
+  "List the public organizations for a user."
+  [user]
+  (api-call :get "users/%s/orgs" [user] nil))
+
+(defn orgs
+  "List the public and private organizations for the currently
+   authenticated user."
+  [options]
+  (api-call :get "user/orgs" nil options))
+
+(defn specific-org
+  "Get a specific organization."
+  [org & [options]]
+  (api-call :get "orgs/%s" [org] options))
+
+(defn edit-org
+  "Edit an organization.
+   Options are:
+      billing-email -- Billing email address.
+      company       -- The name of the company the organization belongs to.
+      email         -- Publically visible email address.
+      location      -- Organization location.
+      name          -- Name of the organization."
+  [org options]
+  (api-call :post "orgs/%s" [org] options))
