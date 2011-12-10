91360990d53a14fd2f431dab29fd90adf4a5398e
Anthony Grimes <i@raynes.me>
2011-12-10 02:54:38 -0600

    Add some tests for core. omgtests!

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[clojure "1.3.0"]
                  [clj-http "0.2.5"]
                  [cheshire "2.0.2"]
-                 [slingshot "0.9.0"]])
+                 [slingshot "0.9.0"]]
+  :dev-dependencies [[midje "1.3.0-RC4"]])
--- test/tentacles/core_test.clj
+++ test/tentacles/core_test.clj
@@ -1,7 +1,29 @@
 (ns tentacles.core-test
-  (:use clojure.test
+  (:use midje.sweet
         tentacles.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact
+ (query-map {:foo 0
+             :foo-bar 0
+             "baz-quux" 0}) => {"foo" 0 "foo_bar" 0 "baz_quux" 0})
+
+(fact
+ (parse-json nil) => nil?
+ (parse-json "{\"foo\" : 0}") => {:foo 0})
+
+(letfn [(status-map [status] {:status status :body 0})]
+  (tabular
+   (fact
+    (safe-parse {:status ?status :body "0"}) => ?expected
+    (provided))
+   ?status ?expected
+   404     (status-map 404)
+   400     (status-map 400)
+   422     (status-map 422)
+   500     (status-map 500)
+   204     (status-map 204)
+   200     0))
+
+(fact
+ (no-content? {:status 204}) => truthy
+ (no-content? {:status 200}) => falsey)
