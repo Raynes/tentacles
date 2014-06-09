19430e91e81d1d2f9d2ccc9b9f4bb0ac2afe9f0e
Bahadir Cambel <bcambel@gmail.com>
2014-06-09 15:03:01 +0200

    bump project version to 0.2.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tentacles "0.2.6"
+(defproject tentacles "0.2.7"
   :description "A library for working with the Github API."
   :url "https://github.com/Raynes/tentacles"
   :license {:name "Eclipse Public License"
