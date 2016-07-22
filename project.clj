621de523d107aa9addbc4604914c3f34474d6503
keithmantell <keithmantell@gmail.com>
2016-07-22 16:06:00 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/Raynes/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "1.0.1"]
-                 [cheshire "5.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.2.0"]
+                 [cheshire "5.6.3"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.0"]
-                 [environ "1.0.0"]])
+                 [environ "1.0.3"]])
