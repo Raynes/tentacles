750796bb449cbedc8bfec693b47c69f482e03683
Peter Monks <pmonks@gmail.com>
2016-11-08 16:18:20 -0800

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.1.0"]
+                 [clj-http "3.3.0"]
                  [cheshire "5.6.3"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.0"]
